3193e977e10a6e646755b70a1ffc9610751f9027
KuznetsovX <Kuznetsovaltf4@gmail.com>
2025-08-04 14:03:07 +0300

    add guild voice states intent, add channels

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
