be6daa35b93ce599b5fc1f82089fc7b4da42e942
KuznetsovX <Kuznetsovaltf4@gmail.com>
2025-09-23 22:25:22 +0300

    feat: add readme-msg-meta, upd centralized db cfg

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import { User, Meta, sequelize, syncDatabase, closeDB } from './connection/index.js';
 import { runBackup, shouldBackup } from './utils/backup-db.js';
+import { saveReadmeMessage, getReadmeMessage } from './utils/readme-message-metadata.js';
 import { removeUserFromDB, removeMembersFromDB } from './utils/remove-members.js';
 import { syncUserToDB, syncMembersToDB } from './utils/sync-members.js';
 import { shouldSyncDB, updateLastSync, getLastSyncMs } from './utils/sync-metadata.js';
@@ -8,6 +9,7 @@ import { getUserRoles, saveUserRoles } from './utils/user-roles.js';
 export {
     User, Meta, sequelize, syncDatabase, closeDB, // Connection
     runBackup, shouldBackup, // Backup Database
+    saveReadmeMessage, getReadmeMessage, // Readme Message
     removeUserFromDB, removeMembersFromDB, // Remove Members
     syncUserToDB, syncMembersToDB, // Sync Members
     shouldSyncDB, updateLastSync, getLastSyncMs, // Sync Metadata
