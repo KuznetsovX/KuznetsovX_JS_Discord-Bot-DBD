512724be8ed348c935b9802ef0b76b593559bcd6
KuznetsovX <Kuznetsovaltf4@gmail.com>
2025-09-25 23:01:08 +0300

    fix: add missing import/export

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,7 @@ import CHANNELS from './channels.js';
 import COMMANDS from './commands.js';
 import MAPS from './maps.js';
 import PRESENCES from './presence.js';
+import ROLE_CATEGORIES from './role-categories.js';
 import ROLE_EMOJIS from './role-emojis.js';
 import ROLES from './roles.js';
 import TILES from './tiles.js';
@@ -28,4 +29,4 @@ export const PREFIXES = ['!', '?'];
 export const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN?.trim();
 export const OWNER_ID = process.env.OWNER_ID?.trim();
 
-export { getReadmeEmbed, CHANNELS, COMMANDS, MAPS, PRESENCES, ROLE_EMOJIS, ROLES, TILES };
+export { getReadmeEmbed, CHANNELS, COMMANDS, MAPS, PRESENCES, ROLE_CATEGORIES, ROLE_EMOJIS, ROLES, TILES };
